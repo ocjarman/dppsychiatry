7dd5ec2f584399e777bed748c28f4899ed4bcfe8
ocjarman <ocjarman@gmail.com>
2023-04-06 09:55:04 -0400

    initialized theme in appjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import {
+  Button,
+  createTheme,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import { orange, purple } from "@mui/material/colors";
 
 function App() {
-  return <div className="App">Welcome!</div>;
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: orange[100],
+      },
+      secondary: {
+        main: purple[100],
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Paper>
+          <Typography variant="h3">Theming Example</Typography>
+          <Typography variant="h3">Hello React</Typography>
+          <h3>magic?</h3>
+          <Button variant="contained" color="secondary">
+            button!
+          </Button>
+        </Paper>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
